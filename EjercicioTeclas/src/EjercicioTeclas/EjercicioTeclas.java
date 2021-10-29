2de3d94632bb5b619586e1780e062908467adccd
EvelinaAB <eweburdzel@gmail.com>
2021-10-29 18:56:48 +0200

    EjercicioTeclas2
    Se ha agregado KeyListener con con sus 3 métodos

--- EjercicioTeclas/src/EjercicioTeclas/EjercicioTeclas.java
+++ EjercicioTeclas/src/EjercicioTeclas/EjercicioTeclas.java
@@ -8,6 +8,8 @@ package EjercicioTeclas;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
@@ -45,16 +47,39 @@ public class EjercicioTeclas {
                 etiqueta.setText("¡Ha pulsado el botón " + cuenta + " veces!");
             }
         });
-
-        ventana.addMouseListener(new MouseAdapter() {
+            botón.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+               
+                System.out.println("Carácter de la tecla: " + e.getKeyChar());
+                
+            }
 
             @Override
-            public void mouseClicked(MouseEvent e) {
-                System.out.println("Pulsado el botón del ratón " + e.getButton());
-                System.out.println("En la posición: " + e.getPoint());
+            public void keyPressed(KeyEvent e) {
+                 System.out.println("Código de la tecla: " + e.getKeyCode());
             }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+               
+                System.out.println("Nombre de la tecla: " + e.getKeyText(e.getKeyCode()));
+            }
+
+          
         });
 
+
+    
+
+//        ventana.addMouseListener(new MouseAdapter() {
+//
+//            @Override
+//            public void mouseClicked(MouseEvent e) {
+//                System.out.println("Pulsado el botón del ratón " + e.getButton());
+//                System.out.println("En la posición: " + e.getPoint());
+//            }
+//        });
         ventana.setSize(600, 400);
         ventana.setVisible(true);
     }
